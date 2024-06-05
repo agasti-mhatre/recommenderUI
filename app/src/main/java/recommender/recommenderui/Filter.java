0218e868e64036c7fe1150a0a7831f281767e5f5
agasti-mhatre <agastimhatre6@gmail.com>
2024-06-05 00:06:25 -0400

    figured out google material view stuff

--- app/src/main/java/recommender/recommenderui/Filter.java
+++ app/src/main/java/recommender/recommenderui/Filter.java
@@ -1,9 +1,6 @@
 package recommender.recommenderui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button tempFilterBtn;
+public class Filter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_filter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-        });
-
-        /*Temporary, delete later*/
-        tempFilterBtn = findViewById(R.id.button);
-        tempFilterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent redirectToFilter = new Intent(MainActivity.this, Filter.class);
-                startActivity(redirectToFilter);
-            }
         });
     }
 }

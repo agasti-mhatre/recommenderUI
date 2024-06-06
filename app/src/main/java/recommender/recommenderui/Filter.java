27276d9643524f6380d309dcafe41259cd2fa926
agasti-mhatre <agastimhatre6@gmail.com>
2024-06-05 23:57:58 -0400

    Last working release before adding http engine

--- app/src/main/java/recommender/recommenderui/Filter.java
+++ app/src/main/java/recommender/recommenderui/Filter.java
@@ -1,6 +1,7 @@
 package recommender.recommenderui;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Filter extends AppCompatActivity {
+
+    private TextInputEditText textInputFoodType;
+    private TextInputEditText textInputRadius;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,24 @@ public class Filter extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textInputFoodType = initTextInputFoodType();
+        textInputRadius = initTextInputRadius();
+
+        submit = initSubmit(textInputFoodType, textInputRadius);
     }
+
+    private TextInputEditText initTextInputFoodType() {
+        return findViewById(R.id.textInputFoodType);
+    }
+
+    private TextInputEditText initTextInputRadius() {
+        return findViewById(R.id.textInputRadius);
+    }
+
+    private Button initSubmit(TextInputEditText... editTexts) {
+
+        return findViewById(R.id.submit);
+    }
+
 }

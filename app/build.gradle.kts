3562ff88deccc12d54b6be371beca619f172e7d5
agasti-mhatre <agastimhatre6@gmail.com>
2024-06-07 21:10:46 -0400

    Added okhttp and removed cronet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.cronet.api)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/recommender/recommenderui/Filter/Filter.java
+++ app/src/main/java/recommender/recommenderui/Filter/Filter.java
@@ -1,7 +1,6 @@
 package recommender.recommenderui.Filter;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -12,12 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-import org.chromium.net.CronetEngine;
-import org.chromium.net.UrlRequest;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
+import okhttp3.OkHttpClient;
 import recommender.recommenderui.R;
 
 public class Filter extends AppCompatActivity {
@@ -53,22 +47,10 @@ public class Filter extends AppCompatActivity {
 
     private Button initSubmit(TextInputEditText... editTexts) {
 
+        OkHttpClient temp = new OkHttpClient();
+
         Button currSubmit = findViewById(R.id.submit);
 
-        CronetEngine cronetEngine = (new CronetEngine.Builder(Filter.this)).build();
-        Executor networkExecutor = Executors.newSingleThreadExecutor();
-
-        UrlRequest.Builder requestBuilder = cronetEngine.newUrlRequestBuilder(
-                "http://localhost:8080/test", new FilterRequestCallback(), networkExecutor
-        );
-
-        UrlRequest getRequest = requestBuilder.build();
-        currSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getRequest.start();
-            }
-        });
 
         return currSubmit;
     }

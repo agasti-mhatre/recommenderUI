0f6f49d63acfda921f3417c5fc9763460ed769b3
agasti-mhatre <agastimhatre6@gmail.com>
2024-07-02 23:58:24 -0400

    Signing off, 7/2/2024

--- app/src/main/java/recommender/recommenderui/view/authenticate/NewAccount.java
+++ app/src/main/java/recommender/recommenderui/view/authenticate/NewAccount.java
@@ -1,6 +1,9 @@
 package recommender.recommenderui.view.authenticate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import recommender.recommenderui.R;
 
 public class NewAccount extends AppCompatActivity {
+
+    private TextInputEditText firstName;
+    private TextInputEditText lastName;
+    private EditText email;
+    private Button submit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +34,29 @@ public class NewAccount extends AppCompatActivity {
             return insets;
         });
 
+        firstName = findViewById(R.id.firstNameInputEditText);
+        lastName = findViewById(R.id.lastNameInputEditText);
+        email = findViewById(R.id.emailAddress);
+        submit = initSubmit();
     }
+
+    private Button initSubmit() {
+
+        Button temp = findViewById(R.id.submit);
+
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendAccountToBackend();
+            }
+        });
+
+        return temp;
+    }
+
+    private void sendAccountToBackend() {
+
+    }
+
 }
